fb58b44b774d5d1daad922e7426fae71b8a60a60
EzeaniChucks <concord_chucks2@yahoo.com>
2023-04-24 01:57:53 -0700

    Succesfullt added dispute description to each concerned requests after dispute submission

--- src/utils/conString.tsx
+++ src/utils/conString.tsx
@@ -1,5 +1,5 @@
-// export const conString = "https://charityorg.onrender.com";
-export const conString = "http://localhost:8080";
+export const conString = "https://charityorg.onrender.com";
+// export const conString = "http://localhost:8080";
 
-// export const conStringFrontEnd = "https://charityorg.vercel.app";
-export const conStringFrontEnd = "http://localhost:3000";
+export const conStringFrontEnd = "https://charityorg.vercel.app";
+// export const conStringFrontEnd = "http://localhost:3000";
